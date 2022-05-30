91a3e584a19e4880ed53437326729115b3bd3e37
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2022-05-30 11:45:38 +0300

    tested api connection

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,19 @@ let characters;
 const container = document.querySelector('.content-section__content');
 
 class loadDataAPI {
-    async getData(url) {
+    async getData(url, start, end) {
         try {
             //try to send a request to server woth API key
             const response = await axios.get(url, {
                 params: {
                     apikey: API_KEY,
-                    limit: 100
+                    offset: start, //start from begining
+                    limit: end // get first 50 records
                 }
             });
-            //return 100 records list of characters
+            //return limit records list of characters
             return response.data.data.results;
+            //return response.data;
         } catch (error) {
             console.log(error.message);
             return false;
@@ -28,25 +30,52 @@ class loadDataAPI {
 
 const loadData = new loadDataAPI();
 
-const data = loadData.getData(url);
+const data = loadData.getData(url, 0, 100);
 
 data.then(data => {
+        //console.log(data);
+
     characters = data;
     console.log(characters);
 
-    for (let i = 0; i < 12; i++) {
-        // console.log(characters[i]);
-        if (characters[i].thumbnail) {
-            const path = characters[i].thumbnail.path;
-            const extension = characters[i].thumbnail.extension;
-            const image = new Image({
-                id: `image-${i}`,
-                selector: `hero-image`,
-                src: `${path}.${extension}`
-            });
-        
-            container.append(image.renderComponent());
-        }
-        
-    }
+    console.log('next 50');
+
+    const next = loadData.getData(url, 101, 100)
+        .then(data => {
+            console.log(data);
+
+            if (characters[65].comics.available > 0) {
+                const comicsURI = characters[65].comics.collectionURI;
+                console.log(characters[65].comics);
+                // loadData.getData(comicsURI, 0, characters[65].comics.available - 1)
+                // .then(data => {
+                //     console.log(data);
+                // });
+            }
+            
+            
+        });
+
+        //const comicsURI = characters[0].comics.collectionURI;
+
+        // loadData.getData(comicsURI)
+        //     .then(data => {
+        //         console.log(data);
+        //     });
+
+        // for (let i = 0; i < 12; i++) {
+        //     // console.log(characters[i]);
+        //     if (characters[i].thumbnail) {
+        //         const path = characters[i].thumbnail.path;
+        //         const extension = characters[i].thumbnail.extension;
+        //         const image = new Image({
+        //             id: `image-${i}`,
+        //             selector: `hero-image`,
+        //             src: `${path}.${extension}`
+        //         });
+            
+        //         container.append(image.renderComponent());
+        //     }
+            
+        // }
 });
